603f1ea4a2ad914ee0c4b216402d855684ca7c0e
fes <fesss32@yandex.ru>
2021-03-19 20:41:34 +0500

    RefactoringCode

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemyPrefab;
+    [SerializeField] private int _countEnemy;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private Player _player;
+
+    private int _countDiedEnemies=0;
+
+    public event UnityAction AllEnemyDied;
+
+    private void OnEnable()
+    {
+        SpawnEnemy();
+    }
+
+    private void SpawnEnemy()
+    {
+        int _currentSpawnPointIndex = 0;
+
+        for (int i = 0; i < _countEnemy; i++)
+        {
+            Enemy spawnedEnemy = Instantiate(_enemyPrefab, transform);
+
+            spawnedEnemy.Spawn(_player);
+            spawnedEnemy.transform.position = _spawnPoints[_currentSpawnPointIndex].position;
+            spawnedEnemy.Dying += EnemyDying;
+
+            _currentSpawnPointIndex++;
+
+            if (_currentSpawnPointIndex >= _spawnPoints.Length)
+                _currentSpawnPointIndex = 0;
+        }
+    }
+
+    private void EnemyDying(Enemy enemy)
+    {
+        enemy.Dying -= EnemyDying;
+        Destroy(enemy.gameObject);
+        _countDiedEnemies++;
+
+        if (_countEnemy == _countDiedEnemies)
+            AllEnemyDied?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        float _currentSpeed = _speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(0, _currentSpeed, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(0, -_currentSpeed, 0);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-_currentSpeed, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(_currentSpeed, 0, 0);
+    }
+}
